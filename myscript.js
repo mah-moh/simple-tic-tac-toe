ed48e35e6c149f43859eda249a0b38a762ee555e
maheen121 <44141162+maheen121@users.noreply.github.com>
2020-04-02 21:20:25 +0600

    Add files via upload

---
+++ myscript.js
@@ -0,0 +1,21 @@
+alert('Hey this Maheen');
+var restart = document.querySelector('#b');
+var table = document.querySelectorAll('td');
+function clear() {
+    for (var i = 0; i < table.length; i++) {
+      table[i].textContent = '';
+    }
+}
+restart.addEventListener('click', clear);
+function change() {
+  if (this.textContent === '') {
+    this.textContent = 'X';
+  }else if (this.textContent === 'X') {
+    this.textContent = 'O';
+  }else {
+    this.textContent = '';
+  }
+}
+for (var i = 0; i < table.length; i++) {
+  table[i].addEventListener('click', change);
+}
